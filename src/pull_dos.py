6218735a08219a33837205ae40aa0b28c590b6c4
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-11-07 16:09:55 +0000

    Extract dates from Derbyshire

--- src/pull_dos.py
+++ src/pull_dos.py
@@ -12,4 +12,12 @@ except:
 # Map to BeautifulSoup
 soup = BeautifulSoup(page, features='lxml')
 
-print(soup)
+#birdnavdates = soup.find_all('div', {'id':'birdnavdate'})
+birdnavs = [(d.getText(), d.find_next('ul', {'id':'birdnav'})) for d in soup.find_all('div', {'id':'birdnavdate'})]
+
+for b in birdnavs:
+    print(b[0])
+    print(b[1])
+    print()
+    print()
+    print()
