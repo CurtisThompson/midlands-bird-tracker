52abea1dfc17f6a6c59673ce1c79a58b1c0a5594
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-11-07 16:15:45 +0000

    Derbyshire into date, location, text

--- src/pull_dos.py
+++ src/pull_dos.py
@@ -12,12 +12,20 @@ except:
 # Map to BeautifulSoup
 soup = BeautifulSoup(page, features='lxml')
 
-#birdnavdates = soup.find_all('div', {'id':'birdnavdate'})
+# Extract tuple of dates and bird text
 birdnavs = [(d.getText(), d.find_next('ul', {'id':'birdnav'})) for d in soup.find_all('div', {'id':'birdnavdate'})]
 
-for b in birdnavs:
-    print(b[0])
-    print(b[1])
-    print()
-    print()
-    print()
+# Split tuples into [date, location, bird text]
+location_sightings = []
+for birddate in birdnavs:
+    date = birddate[0]
+    fulltext = birddate[1]
+
+    # Split location and text
+    list_items = fulltext.find_all('li')
+    for li in list_items:
+        location = li.find('strong').getText()
+        birdtext = li.getText().replace(location, '').strip()
+        location = location.strip()
+        location_sightings.append([date, location, birdtext])
+        print([date, location, birdtext])
