7eabef9368820cee74edc2afd1b7b4a661966240
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-11-08 15:04:51 +0000

    Tidy initialise code

--- src/initialise/setup_repo.py
+++ src/initialise/setup_repo.py
@@ -1,5 +1,10 @@
 import os
 
-# Create required directories
-os.makedirs('./data/scrape_extracts/', exist_ok=True)
-os.makedirs('./reports/', exist_ok=True)
+
+def initialise_repository():
+    os.makedirs('./data/scrape_extracts/', exist_ok=True)
+    os.makedirs('./reports/', exist_ok=True)
+
+
+if __name__ == "__main__":
+    initialise_repository()
