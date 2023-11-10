68a63ab73425b96781c2e005731ac45d27c4d5bc
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-11-10 11:32:32 +0000

    Set up repo entry point changed to run

--- src/initialise/setup_repo.py
+++ src/initialise/setup_repo.py
@@ -1,10 +1,10 @@
 import os
 
 
-def initialise_repository():
+def run():
     os.makedirs('./data/scrape_extracts/', exist_ok=True)
     os.makedirs('./reports/', exist_ok=True)
 
 
 if __name__ == "__main__":
-    initialise_repository()
+    run()
